9e5894abdfc9b8b1488f25a27f12220690127b97
Nithin Sai Potnuru <147067175+nithin32880@users.noreply.github.com>
2025-01-24 10:01:14 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+// Save this file as HelloWorld.java
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
